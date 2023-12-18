4f9e31a0c81b9bdcca41310dd1c8057fc8af0cbd
Anton Fahlstedt <antonfahlstedt@live.com>
2023-12-18 23:36:34 +0100

    Initial Commit

---
+++ UPG3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UPG3
+//
+//  Created by Anton Fahlstedt on 2023-12-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UPG3/UPG3App.swift
@@ -0,0 +1,17 @@
+//
+//  UPG3App.swift
+//  UPG3
+//
+//  Created by Anton Fahlstedt on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct UPG3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
